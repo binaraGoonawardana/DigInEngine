1bcbf76c02a1a3bfccda101fff9cd74f0b3d385c
marlonabeykoon <ronnie@duosoftware.com>
2015-10-01 18:03:02 +0530

    new files

---
+++ scripts/LogicImplementor/LogicImplementor.py
@@ -0,0 +1,8 @@
+__author__ = 'Marlon'
+
+#import sys
+#sys.path.insert(0,'/digin-engine/modules')
+
+#import_file('C:/Users/Administrator/PycharmProjects/digin-engine/modules/')
+import ObjectStoreDataHandler
+
