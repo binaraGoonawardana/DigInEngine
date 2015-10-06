a41b6cca6ec58fe4471aaf7516254433c8f24ff8
sajeetharan <sajeetharan@duosoftware.com>
2015-10-06 15:45:58 +0530

    Big query handler added ( To retrieve data by passing custom query)

--- modules/GetFields.py
+++ modules/GetFields.py
@@ -2,16 +2,16 @@ __author__ = 'Sajeetharan'
 import web
 from bigquery import get_client
 urls = (
-    '/engine/(.*)', 'execute_query'
-
+    '/GetFields(.*)', 'get_Fields'
 )
 app = web.application(urls, globals())
-query = ""
-class execute_query:
-    def GET(self,querystate):
-          query = querystate
+class get_Fields:
+    def GET(self,r):
+          fields = []
+          datasetname = web.input().datasetName
+          tablename = web.input().tableName
 # BigQuery project id as listed in the Google Developers Console.
-          project_id = 'digin-1085'
+          project_id = 'publicdata'
 # Service account email address as listed in the Google Developers Console.
           service_account = '1011363222700-epqo6lmkl67j6u1qafha9dlke0pmcck3@developer.gserviceaccount.com'
 # PKCS12 or PEM key provided by Google.
@@ -19,11 +19,12 @@ class execute_query:
           client = get_client(project_id, service_account=service_account,
                             private_key_file=key, readonly=True)
         # Submit an async query.
-          job_id, _results = client.query(query)
-        # Check if the query has finished running.
-          complete, row_count = client.check_job(job_id)
-        # Retrieve the results.
-          results = client.get_query_rows(job_id)
-          return results
+         # job_id, _results = client.query("SELECT  * FROM" + datasetname+".INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME ="+tablename)
+        # Submit an async query.
+          results = client.get_table_schema(datasetname,tablename)
+          for x in results:
+            fields.append(x['name'])
+          return fields
+
 if  __name__ == "__main__":
     app.run()
