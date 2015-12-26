9b51b500bc5c35c5c57aa89e79f8dd7716624d9f
marlonabeykoon <ronnie@duosoftware.com>
2015-12-26 10:02:13 +0530

    Initializing of background process completed

--- scripts/SocialMediaService/StreamingAnalytics.py
+++ scripts/SocialMediaService/StreamingAnalytics.py
@@ -5,7 +5,22 @@ import tweepy
 import sys
 import pika
 import json
+import logging
 import time
+import traceback
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.FileHandler('social_media_analytrics.log')
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+logger.info('--------------------------------------  SocialMediaService  ---------------------------------------------')
+logger.info('Starting log')
 
 consumer_key = "xHl7DEIJjH8pNM2kn8Q9EddGy"
 consumer_secret = "eVxjTk7d4Z41VQ2Kt7kcOF6aFjTQqqiWIKgM8xhqkMYoE8Pxmq"
@@ -16,10 +31,11 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-def start_listner(unique_id, keyword, limit):
-    class CustomStreamListener(tweepy.StreamListener):
+def start_listner(unique_id, keyword, limit=200):
 
+    class CustomStreamListener(tweepy.StreamListener):
         def __init__(self, api):
+            logger.info('runnning')
             self.api = api
             super(tweepy.StreamListener, self).__init__()
 
@@ -30,18 +46,19 @@ def start_listner(unique_id, keyword, limit):
             self.channel = connection.channel()
 
             #set max queue size
-            args = {"x-max-length": limit}
+            args = {"x-max-length": 200}
 
             self.channel.queue_declare(queue=unique_id, arguments=args)
 
         def on_status(self, status):
-            print status.text.encode('utf-8'), "\n"
+            #print status.text.encode('utf-8'), "\n"
 
             data = {}
             data['text'] = status.text.encode('utf-8')
             data['created_at'] = time.mktime(status.created_at.timetuple())
             data['geo'] = status.geo
             data['source'] = status.source
+            logger.info(data)
 
             #queue the tweet
             self.channel.basic_publish(exchange='',
@@ -49,13 +66,49 @@ def start_listner(unique_id, keyword, limit):
                                         body=json.dumps(data))
 
         def on_error(self, status_code):
-            print >> sys.stderr, 'Encountered error with status code:', status_code
+            #print >> sys.stderr, 'Encountered error with status code:', status_code
             return True  # Don't kill the stream
 
         def on_timeout(self):
-            print >> sys.stderr, 'Timeout...'
+            #print >> sys.stderr, 'Timeout...'
             return True  # Don't kill the stream
 
+    #f = open("Output.txt","r")
+    #keyword = f.read()
+    #logger.info(keyword)
+    #f.close()
     sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
+    try:
+        logger.info('tracking started')
+        logger.info(keyword)
+        kw = keyword
+        sapi.filter(track=[kw])
+    except Exception, err:
+        logger.info(keyword)
+        logger.info(traceback.format_exc())
 
-    sapi.filter(track=[keyword])
+# if __name__ == "__main__":
+#     logger.info("just now started")
+#     # try:
+#         # a = str(sys.argv[1])
+#         #b = str(sys.argv[2])
+#         #c = int(sys.argv[5])
+#         #logger.info(a)
+#     #     logger.info(b)
+#     # except Exception, err:
+#     #     logger.info("inside main")
+#
+#     start_listner()
+#     # start_listner('nokia_queue', 'Nokia', limit=200)
+if __name__ == "__main__":
+    logger.info("just now started")
+    try:
+        a = str(sys.argv[1])
+        b = str(sys.argv[2])
+        #c = int(sys.argv[5])
+        logger.info(a)
+        logger.info(b)
+    except Exception, err:
+        logger.exception("inside main")
+    else:
+        start_listner(a, b)
