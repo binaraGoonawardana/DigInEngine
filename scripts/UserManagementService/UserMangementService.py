afedac89c2e9bf9eb21ff2d1f5cb5914eece9c09
marlonabeykoon <ronnie@duosoftware.com>
2016-05-04 17:26:10 +0530

    Bug fix UserManagementService

--- scripts/UserManagementService/UserMangementService.py
+++ scripts/UserManagementService/UserMangementService.py
@@ -1,4 +1,5 @@
 __author__ = 'Marlon Abeykoon'
+__version__ = '1.1.0.0'
 
 import scripts.DigINCacheEngine.CacheController as CC
 import modules.CommonMessageGenerator as cmg
@@ -23,15 +24,35 @@ def store_user_settings(params):
              'email': params['email'],
              'components': params['components'],
              'user_role': params['user_role'],
-             'refresh_interval': params['refresh_interval'],
-             'widget_limit': params['widget_limit'],
-             'query_limit': params['query_limit'],
+             'refresh_interval': int(params['refresh_interval']),
+             'widget_limit': int(params['widget_limit']),
+             'query_limit': int(params['query_limit']),
              'image_path': params['image_path'],
              'theme_config': params['theme_config'],
-             'created_date_time' : datetime.datetime.now()
+             'modified_date_time': datetime.datetime.now(),
+             'created_date_time': datetime.datetime.now()
              }]
     logger.info("Data received!")
     logger.info(data_object)
+    existance = CC.get_data("SELECT user_id from digin_user_settings where user_id = '{0}'".format(params['user_id']))
+    if existance['rows'] != ():
+        try:
+            CC.update_data('digin_user_settings',"WHERE user_id='{0}'".format(params['user_id']),
+                           components=params['components'],
+                           user_role=params['user_role'],
+                           refresh_interval=int(params['refresh_interval']),
+                           widget_limit=int(params['widget_limit']),
+                           query_limit=int(params['query_limit']),
+                           image_path=params['image_path'],
+                           theme_config=params['theme_config'],
+                           modified_date_time=datetime.datetime.now())
+            return cmg.format_response(True,1,"User settings updated successfully")
+        except Exception, err:
+            logger.error("Error updating user settings")
+            logger.error(err)
+            print "Error updating user settings"
+            print err
+            raise
     try:
         CC.insert_data(data_object,'digin_user_settings')
     except Exception, err:
@@ -43,7 +64,7 @@ def store_user_settings(params):
     return cmg.format_response(True,1,"User settings saved successfully")
 
 def get_user_settings_by_id(params):
-    query = "SELECT * FROM digin_user_settings WHERE user_id = {0}".format(int(params.user_id))
+    query = "SELECT * FROM digin_user_settings WHERE user_id = {0}".format(params.user_id)
     try:
         user_data = CC.get_data(query)
     except Exception, err:
