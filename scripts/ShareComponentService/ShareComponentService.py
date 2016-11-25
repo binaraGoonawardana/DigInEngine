af7153b6b62d8ec2ba1f264b54344724f4be2fd4
marlonabeykoon <marlonabeykoon@gmail.com>
2016-11-25 15:05:27 +0530

    fix

--- scripts/ShareComponentService/ShareComponentService.py
+++ scripts/ShareComponentService/ShareComponentService.py
@@ -1,7 +1,9 @@
 __author__ = 'Marlon Abeykoon'
-__version__ = '1.0.1.0'
+__version__ = '1.0.1.1'
 
+import json
 import InternalSharing as intshare
+import modules.CommonMessageGenerator as cmg
 
 class ShareComponent():
 
@@ -10,6 +12,16 @@ class ShareComponent():
         self.data = kwargs
 
     def share_component(self):
+
         if self.method == 'component_internal':
-            return intshare.InternalSharing(self.data['security_level_auth'], self.data['comp_type'], self.data['share_data'],
-                                            self.data['UserID'], self.data['Domain']).do_share()
+            share_obj = intshare.InternalSharing(self.data['security_level_auth'], self.data['comp_type'],# self.data['share_data'],
+                                            self.data['UserID'], self.data['Domain'])
+            share_result = share_obj.do_share(self.data['share_data'])
+            if not json.loads(share_result)['Is_Success']:
+                return share_result
+            unshare_result = share_obj.undo_share(self.data['unshare_data'])
+            if not json.loads(unshare_result)['Is_Success']:
+                return unshare_result
+
+            return cmg.format_response(True, {'successful_shares':share_obj.authorized_shares, 'unsuccessful_shares':share_obj.unauthorized_shares},
+                                       'Components sharing process successful')
