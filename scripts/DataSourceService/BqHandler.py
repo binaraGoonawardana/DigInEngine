d42adbf1dcbf16bba42234b6da2cc539f84a5780
Thivatharan Jeganathan <jeganathan@duosoftwer.com>
2016-12-07 20:29:33 +0530

    bug fixed in datasource deletion. and SQLQueryHandler.py changes

--- scripts/DataSourceService/BqHandler.py
+++ scripts/DataSourceService/BqHandler.py
@@ -12,10 +12,11 @@ datasource_settings = conf.get_conf('DatasourceConfig.ini','BIG-QUERY')
 project_id = datasource_settings['PROJECT_ID']
 service_account = datasource_settings['SERVICE_ACCOUNT']
 key = datasource_settings['KEY']
-credentials = ServiceAccountCredentials.from_p12_keyfile(service_account, key)
+
 
 def sync_query(dataset_id,table_id,first_row_no,last_row_no,upload_id):
 
+    credentials = ServiceAccountCredentials.from_p12_keyfile(service_account, key)
     query = "DELETE FROM {0}.{1} WHERE _index_id >= {2} AND _index_id < {3}".format(dataset_id,table_id,first_row_no,last_row_no)
     client = gcb.Client(project=project_id,credentials=credentials)
     query_results = client.run_sync_query(query)
