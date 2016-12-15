663c93190a94edb25b813b2897fb5a85a5c6d939
omalbhagya <bhagya.omal@gmail.com>
2016-12-15 17:18:07 +0530

    version upgrades in descriptive analytic service, descriptive analytic processor, ForecastingEsService and ForecastingEsProcessor

--- scripts/DescriptiveAnalyticsService/DescriptiveAnalyticsService.py
+++ scripts/DescriptiveAnalyticsService/DescriptiveAnalyticsService.py
@@ -1,5 +1,5 @@
 __author__ = 'Marlon'
-__version__ = '1.0.1.1'
+__version__ = '1.0.1.2'
 
 import sys,os
 sys.path.append("...")
--- scripts/PredictiveAnalysisEngine/ForecastingEsProcessor.py
+++ scripts/PredictiveAnalysisEngine/ForecastingEsProcessor.py
@@ -1,5 +1,5 @@
 __author__ = 'Manura Omal Bhagya'
-__version__ = '1.0.3.4'
+__version__ = '1.0.3.5'
 
 import sys
 sys.path.append("...")
