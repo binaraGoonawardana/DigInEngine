8552371868a969524d01a7a357824e0885a32768
marlonabeykoon <marlonabeykoon@gmail.com>
2016-12-23 12:11:47 +0530

    alias fix

--- scripts/AggregationEnhancer/CommonFormulaeGenerator.py
+++ scripts/AggregationEnhancer/CommonFormulaeGenerator.py
@@ -5,7 +5,9 @@ def get_func(db, field_name, aggregator):
 
     if aggregator.lower() == 'percentage':
         if db.lower() == 'mssql':
-            func = 'COUNT({0}) * 100.0 / SUM(COUNT({1})) OVER() as percentage_{2}'.format(field_name, field_name,field_name)
+            func = 'COUNT({0}) * 100.0 / SUM(COUNT({1})) OVER() as percentage_{2}'.format(field_name, field_name,
+                                                                                          filter(lambda x: x not in '[] ',
+                                                                                                 field_name))
             return func
         elif db.lower() == 'bigquery':
             func = 'COUNT ({0}) as total, RATIO_TO_REPORT(total) OVER() as percentage_{1}'.format(field_name, field_name)
@@ -14,17 +16,17 @@ def get_func(db, field_name, aggregator):
             func = 'COUNT({0}) * 100.0 / SUM(COUNT({1})) OVER() as percentage_{2}'.format(field_name, field_name,field_name)
             return func
     if aggregator.lower() == 'avg':
-        func = 'avg({0}) as avg_{1}'.format(field_name, field_name)
+        func = 'avg({0}) as avg_{1}'.format(field_name, filter(lambda x: x not in '[] ',field_name))
         return func
     if aggregator.lower() == 'sum':
-        func = 'sum({0}) as sum_{1}'.format(field_name, field_name)
+        func = 'sum({0}) as sum_{1}'.format(field_name, filter(lambda x: x not in '[] ', field_name))
         return func
     if aggregator.lower() == 'count':
-        func = 'count({0}) as count_{1}'.format(field_name, field_name)
+        func = 'count({0}) as count_{1}'.format(field_name, filter(lambda x: x not in '[] ', field_name))
         return func
     if aggregator.lower() == 'max':
-        func = 'max({0}) as max_{1}'.format(field_name, field_name)
+        func = 'max({0}) as max_{1}'.format(field_name, filter(lambda x: x not in '[] ', field_name))
         return func
     if aggregator.lower() == 'min':
-        func = 'min({0}) as min_{1}'.format(field_name, field_name)
+        func = 'min({0}) as min_{1}'.format(field_name, filter(lambda x: x not in '[] ', field_name))
         return func
